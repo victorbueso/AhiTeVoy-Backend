4a6c16ad24b02d2465aae1424e92b624ca846559
jdfiallos <juandanielfiallos@gmail.com>
2021-07-15 14:16:22 -0600

    [ADD] Carpet public with index

--- index.js
+++ index.js
@@ -1,34 +1,24 @@
 const express = require('express');
 let bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config();
 
-/* Definicion de Routes */
+//Definicion de Routes
 let motoristarRouter = require('./routes/auth-motorista');
 
 var app = express();
-app.use(cors());
 
 //Middleware (Debe ingresarse respectivamente en el orden establecido.)
-
+app.use(cors());
 app.use(express.static('public'));
 app.set('port', process.env.PORT || 3000);
 //Body-Parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-//Lectura
-app.use( express.json() );
 //Rutas
 app.use('/auth/motorista', motoristarRouter);
 
 
-app.get('/', ( req, res )=>{
-    res.json({
-        ok: true,
-        msg: 'Todo salio bien'
-    })
-});
-
-//Levantar server.
 app.listen(process.env.PORT || 3000, () => {
     console.log(`App listening on ${process.env.PORT || 3000} port!`);
 });

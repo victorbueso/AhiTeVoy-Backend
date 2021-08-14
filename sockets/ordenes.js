654f8a11a9318b914b80fbc4a03158842159c427
jdfiallos <juandanielfiallos@gmail.com>
2021-08-13 18:08:30 -0600

    [UPDATE] Orders Socket

--- sockets/ordenes.js
+++ sockets/ordenes.js
@@ -1,6 +1,29 @@
+const { io } = require('../index')
+const Ordenes = require('../models/ordenes')
 
+
+//Aqui quitaremos a lo usuarios que se desconecten.
 const desconectar = async ( req ) =>{
-    console.log('Usuario desconectado.')
+    console.log('Usuario desconectado. NODE.')
 } 
 
-module.exports = desconectar;
+//Escuchamos las peticiones
+const mensaje = async ( payload ) =>{
+    console.log('TU ORDEN');
+    let ordenes = await Ordenes.find({"tomada": false});
+
+    io.to('Motoristas-salas').emit('orden-tomada', ordenes);
+}
+
+//Escuchamos las peticiones
+const confUsuario = async ( payload ) =>{
+    //Aqui agregamos a los usuarios conectados.
+    console.log('Configurando usuario', payload);
+    io.emit('user-configurado', payload);
+}
+
+module.exports = {
+    desconectar,
+    mensaje,
+    confUsuario
+};
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,11 +1,27 @@
-const desconectar = require('./ordenes');
+const { desconectar, mensaje, confUsuario } = require('./ordenes');
 const { io } = require('../index');
 
-io.on('connection', cliente => {
-    console.log( 'Usuario conectado' );
+io.on('connection', (cliente, io) => {
 
+    //Extraemos el id unico del cliente
+    const idHandShake = cliente.id;
+    
+    //Extraemos el nombre de la sala
+    const nameRoom  = 'Motoristas-salas';
+
+    //Conectamos cliente al sala.
+    cliente.join(nameRoom);
+ 
+    //Configurar usuario conectado
+    cliente.on('configurar-usuario', confUsuario) 
+
+    console.log(`Hola dipositivo: ${idHandShake} se unio a la sala ${nameRoom}`);
+    //Desconectar
     cliente.on('disconnect', desconectar);
 
+    //Con esto escuchamos las emisiones ordenes del cliente.
+    cliente.on('ordenes', mensaje );
+    
 })
 
-io.emit('connection');
+io.emit('connection'); 
